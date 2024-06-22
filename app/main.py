959c464dbe5ab42d13bb02ce7a84b149612ef5c2
kevin <kevinxu360@outlook.com>
2024-06-21 17:43:03 -0700

    name change

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.queries.user_queries import router as query_router
 from app.api.commands.user_commands import router as command_router
 
-app = FastAPI()
+app = FastAPI(title="Healthcare Analytics Microservice", version="0.0.1")
 
 app.include_router(query_router, prefix="/query", tags=["Queries"])
 app.include_router(command_router, prefix="/command", tags=["Commands"])
