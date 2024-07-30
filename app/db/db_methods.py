2d08c25f86140b5eb192eef8b1383237f06e46ff
kevin <kevinxu360@outlook.com>
2024-07-29 21:23:09 -0700

    setting up configs

--- app/db/db_methods.py
+++ app/db/db_methods.py
@@ -1,34 +1,5 @@
-import azure.cosmos.documents as documents
-import azure.cosmos.cosmos_client as cosmos_client
+from database import db, container
 import azure.cosmos.exceptions as exceptions
-from azure.cosmos.partition_key import PartitionKey
-import datetime
-import config
-
-HOST = config.settings['host']
-MASTER_KEY = config.settings['master_key']
-DATABASE_ID = config.settings['database_id']
-CONTAINER_ID = config.settings['container_id']
-
-client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY})
-
-if __name__ == "__main__":
-    # setup database
-    try:
-        db = client.create_database(id=DATABASE_ID)
-        print('Database with id \'{0}\' created'.format(DATABASE_ID))
-    except exceptions.CosmosResourceExistsError:
-        db = client.get_database_client(DATABASE_ID)
-        print('Database with id \'{0}\' was found'.format(DATABASE_ID))
-
-    # setup container
-    try:
-        container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
-        print('Container with id \'{0}\' created'.format(CONTAINER_ID))
-    except exceptions.CosmosResourceExistsError:
-        container = db.get_container_client(CONTAINER_ID)
-        print('Container with id \'{0}\' was found'.format(CONTAINER_ID))
-
 
 def scale_container(container):
     print('\nScaling Container\n')
@@ -45,7 +16,6 @@ def scale_container(container):
             print(e.http_error_message)
         else:
             raise e
-
 
 def profile_schema(id, name, age, params, risk):
     profile = {
@@ -80,5 +50,3 @@ def create_profile():
     except exceptions.CosmosHttpResponseError as e:
         print('An error occurred while querying or creating the profile.')
         print(e.http_error_message)
-
-
