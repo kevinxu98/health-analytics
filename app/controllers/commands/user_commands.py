fe27012874b97927aa17d3c6f74024612dd98fa0
kevin <kevinxu360@outlook.com>
2024-07-30 14:45:17 -0700

    fix debug

--- app/controllers/commands/user_commands.py
+++ app/controllers/commands/user_commands.py
@@ -11,7 +11,8 @@ async def test_command_endpoint():
 @router.post("/add_profile")
 async def add_profile(user_profile: ProfileDTO, name: str = Query()):
     try:
-        profile_page = create_profile(name, user_profile.dict())
+        create_profile(name, user_profile.dict())
         return {"message": "profile added successfully!"}
     except Exception as e:
         return {"error": str(e)}
+    
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from app.controllers.queries.user_queries import router as query_router
 from app.controllers.commands.user_commands import router as command_router
 
@@ -6,3 +7,6 @@ app = FastAPI(title="Healthcare Analytics Microservice", version="0.0.1")
 
 app.include_router(query_router, prefix="/query", tags=["Queries"])
 app.include_router(command_router, prefix="/command", tags=["Commands"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
